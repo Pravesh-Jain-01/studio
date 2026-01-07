53a00b8e90e21b747e8d8bd3c029ebdda9317b10
Pravesh Jain <praveshjain0107@gmail.com>
2026-01-07 17:49:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7819995370-4cf89",
+  "appId": "1:1030104328536:web:82ad87917d53179dcd9d2d",
+  "apiKey": "AIzaSyCfUKJ6qhNBuwCpNwqpU9Xo-GH43grjzVo",
+  "authDomain": "studio-7819995370-4cf89.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1030104328536"
+};
