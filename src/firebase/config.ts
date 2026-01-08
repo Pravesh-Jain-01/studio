033f8e429217bc7fb16ed4c593cd47da2a31eacd
Pravesh Jain <praveshjain0107@gmail.com>
2026-01-08 12:09:41 +0000

    this is coming when adding the product

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1030104328536:web:82ad87917d53179dcd9d2d",
   "apiKey": "AIzaSyCfUKJ6qhNBuwCpNwqpU9Xo-GH43grjzVo",
   "authDomain": "studio-7819995370-4cf89.firebaseapp.com",
+  "storageBucket": "studio-7819995370-4cf89.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1030104328536"
 };
